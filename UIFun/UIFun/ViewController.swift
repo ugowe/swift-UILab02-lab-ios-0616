c488400781e1ea4e5a26ae0e5fa1181d115150ba
Michael Dippery <michael@monkey-robot.com>
2016-06-19 15:53:22 -0700

    Complete project

--- UIFun/UIFun/ViewController.swift
+++ UIFun/UIFun/ViewController.swift
@@ -9,4 +9,46 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet weak var paintBucket: UIView!
+    @IBOutlet weak var firstColor: UISegmentedControl!
+    @IBOutlet weak var secondColor: UISegmentedControl!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        paintBucket.paintColorName = "red"
+    }
+
+    func mixColors(withFirst first: String, second: String) -> String {
+        switch first.lowercaseString {
+        case "red":
+            switch second.lowercaseString {
+            case "red": return "red"
+            case "yellow": return "orange"
+            case "blue": return "purple"
+            default: assert(false, "Invalid colors")
+            }
+        case "yellow":
+            switch second.lowercaseString {
+            case "red": return "orange"
+            case "yellow": return "yellow"
+            case "blue": return "green"
+            default: assert(false, "Invalid colors")
+            }
+        case "blue":
+            switch second.lowercaseString {
+            case "red": return "purple"
+            case "yellow": return "green"
+            case "blue": return "blue"
+            default: assert(false, "Invalid colors")
+            }
+        default:
+            assert(false, "Invalid colors")
+        }
+    }
+
+    @IBAction func colorSelected(sender: UISegmentedControl) {
+        print("First color: \(firstColor.color.name)")
+        print("Second color: \(secondColor.color.name)")
+        paintBucket.paintColorName = mixColors(withFirst: firstColor.color.name, second: secondColor.color.name)
+    }
 }
