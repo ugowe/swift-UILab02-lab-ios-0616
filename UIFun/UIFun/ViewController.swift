ca5816a2c7505ab673215297198ef04227b5c4e8
ugowe <joe@spheares.com>
2016-07-08 20:10:08 -0400

    add code to Paint lab

--- UIFun/UIFun/ViewController.swift
+++ UIFun/UIFun/ViewController.swift
@@ -9,19 +9,79 @@
 import UIKit
 
 class ViewController: UIViewController {
-    // TODO: Set up IB outlets
-
+    @IBOutlet weak var paintBucket: UIImageView!
+    @IBOutlet weak var firstSegment: UISegmentedControl!
+    @IBOutlet weak var secondSegment: UISegmentedControl!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // TODO: Set the initial paint color to "red"
+        paintBucket.backgroundColor = UIColor.redColor()
     }
 
     func mixColors(withFirst first: String, second: String) -> String {
         // TODO: Mix colors and return a string indicating the name of the mixed color
         // (e.g., "red", "purple", "blue")
+        
+        switch (first, second) {
+        case ("red", "blue"), ("blue", "red"):
+            return "purple"
+        case ("red", "yellow"), ("yellow", "red"):
+            return "orange"
+        case ("yellow", "blue"), ("blue", "yellow"):
+            return "green"
+        case ("red", "red"):
+            return "red"
+        case ("yellow", "yellow"):
+            return "yellow"
+        case ("blue", "blue"):
+            return "blue"
+        default:
+            return "red"
+        }
+        
     }
 
     @IBAction func colorSelected(sender: UISegmentedControl) {
         // TODO: Mix each selected color and set the paint color to the mixed color
+        let firstControl = firstSegment
+        let secondControl = secondSegment
+        
+        var firstPaintColor = ""
+        var secondPaintColor = ""
+        var finalPaintColor = ""
+        
+        
+        switch firstControl.selectedSegmentIndex {
+        case 0:
+            firstPaintColor = "red"
+        case 1:
+            firstPaintColor = "yellow"
+        case 2:
+            firstPaintColor = "blue"
+        default:
+            break
+        }
+        
+        switch secondControl.selectedSegmentIndex {
+        case 0:
+            secondPaintColor = "red"
+        case 1:
+            secondPaintColor = "yellow"
+        case 2:
+            secondPaintColor = "blue"
+        default:
+            break
+        }
+        
+        finalPaintColor = mixColors(withFirst: firstPaintColor, second: secondPaintColor)
+        
+        paintBucket.paintColorName = finalPaintColor
+        
+    
+        
+        
+        
+        
+        
     }
 }
